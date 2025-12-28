a032c7ad0ecec091ec989d4c2bf7b92d3bf68fc2
areebw1-prog <areebwaqar1@gmail.com>
2025-12-29 02:48:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import "./styles.css";
 import Home from "./Home";
-
 import Search from "./Search";
 import { LoginContex } from "./Context";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 
 export default function App() {
-  const [fvalue, setValue] = useState(" ");
+  const [fvalue, setValue] = useState(""); // Shared search value
+
   return (
     <div className="App">
-      <LoginContex.Provider value={{ fvalue, setValue }}></LoginContex.Provider>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="search" element={<Search />} />
-      </Routes>
+      {/* Move the Routes INSIDE the Provider */}
+      <LoginContex.Provider value={{ fvalue, setValue }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="search" element={<Search />} />
+        </Routes>
+      </LoginContex.Provider>
     </div>
   );
 }
