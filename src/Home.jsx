9d66003f59d85fcdd5bd2d91e2ca2e9d286bf495
areebw1-prog <areebwaqar1@gmail.com>
2025-12-29 03:16:57 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -16,7 +16,7 @@ export default function Home() {
           method: "GET",
           headers: {
             "X-RapidAPI-Key":
-              "process.env.REACT_APP_RAPIDAPI_KEY",
+              process.env.REACT_APP_RAPIDAPI_KEY,
             "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
           },
         };
