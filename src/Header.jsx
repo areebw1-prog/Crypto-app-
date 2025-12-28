bee11a88c27d015bfb258604477e1a064d57b2ea
areebw1-prog <areebwaqar1@gmail.com>
2025-12-29 03:59:49 +0530

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,1 +1,17 @@
+import React from "react";
+import "./styles.css";
 
+export default function Header() {
+  return (
+    <header className="main-header">
+      <div className="logo-section">
+        <img 
+          src="https://rapidapi-prod-apis.s3.amazonaws.com/c3736925-9c98-4fdc-857e-07c11146afb7.png" 
+          alt="Logo" 
+          className="header-logo"
+        />
+        <h1 className="header-title">CryptoCurrency</h1>
+      </div>
+    </header>
+  );
+}
